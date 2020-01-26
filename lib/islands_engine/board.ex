6d44492c4b4c8876cbce7589295317091d93eead
Kamil <paryzowy@gmail.com>
2020-01-26 01:49:43 +0100

    add all islands placed check

--- lib/islands_engine/board.ex
+++ lib/islands_engine/board.ex
@@ -4,9 +4,17 @@ defmodule IslandsEngine.Board do
 
   def position_island(board, key, %Island{} = island) do
     case overlaps_existing_island?(board, key, island) do
-      false -> {:ok, Map.put(board, key, island)}
+      false -> Map.put(board, key, island)
       true -> {:error, :overlapping_island}
     end
+  end
+
+  def all_islands_positioned?(board) do
+    Enum.all?(Island.types(), &Map.has_key?(board, &1))
+  end
+
+  def guess(board, coordinate) do
+    {:miss, :none, :no_win, board}
   end
 
   defp overlaps_existing_island?(board, new_key, new_island) do
--- test/islands_engine/board_test.exs
+++ test/islands_engine/board_test.exs
@@ -11,7 +11,7 @@ defmodule IslandsEngine.BoardTest do
 
     {:ok, dot_island} = Island.new(:dot, coordinate_1)
     {:ok, square_island} = Island.new(:square, coordinate_1)
-    {:ok, board} = Board.new() |> Board.position_island(:dot, dot_island)
+    board = Board.new() |> Board.position_island(:dot, dot_island)
 
     assert Board.position_island(board, :square, square_island) ==
              {:error, :overlapping_island}
@@ -23,9 +23,103 @@ defmodule IslandsEngine.BoardTest do
 
     {:ok, dot_island} = Island.new(:dot, coordinate_1)
     {:ok, square_island} = Island.new(:square, coordinate_2)
-    {:ok, board} = Board.new() |> Board.position_island(:dot, dot_island)
+    board = Board.new() |> Board.position_island(:dot, dot_island)
 
-    assert {:ok, %{dot: ^dot_island, square: ^square_island}} =
+    assert %{dot: ^dot_island, square: ^square_island} =
              Board.position_island(board, :square, square_island)
+  end
+
+  test "one island is not enough to start the game" do
+    {:ok, coordinate_1} = Coordinate.new(1, 1)
+    {:ok, dot_island} = Island.new(:dot, coordinate_1)
+    board = Board.new() |> Board.position_island(:dot, dot_island)
+    assert false == Board.all_islands_positioned?(board)
+  end
+
+  test "two islands are not enough to start the game" do
+    {:ok, coordinate_1} = Coordinate.new(1, 1)
+    {:ok, coordinate_2} = Coordinate.new(1, 2)
+    {:ok, dot_island} = Island.new(:dot, coordinate_1)
+    {:ok, square_island} = Island.new(:square, coordinate_2)
+
+    board =
+      Board.new()
+      |> Board.position_island(:dot, dot_island)
+      |> Board.position_island(:square, square_island)
+
+    assert false == Board.all_islands_positioned?(board)
+  end
+
+  test "three islands are not enough to start the game" do
+    {:ok, coordinate_1} = Coordinate.new(1, 1)
+    {:ok, coordinate_2} = Coordinate.new(1, 2)
+    {:ok, coordinate_3} = Coordinate.new(1, 4)
+    {:ok, dot_island} = Island.new(:dot, coordinate_1)
+    {:ok, square_island} = Island.new(:square, coordinate_2)
+    {:ok, atoll_island} = Island.new(:atoll, coordinate_3)
+
+    board =
+      Board.new()
+      |> Board.position_island(:dot, dot_island)
+      |> Board.position_island(:square, square_island)
+      |> Board.position_island(:atoll, atoll_island)
+
+    assert false == Board.all_islands_positioned?(board)
+  end
+
+  test "four islands are not enough to start the game" do
+    {:ok, coordinate_1} = Coordinate.new(1, 1)
+    {:ok, coordinate_2} = Coordinate.new(1, 2)
+    {:ok, coordinate_3} = Coordinate.new(1, 4)
+    {:ok, coordinate_4} = Coordinate.new(1, 6)
+    {:ok, dot_island} = Island.new(:dot, coordinate_1)
+    {:ok, square_island} = Island.new(:square, coordinate_2)
+    {:ok, atoll_island} = Island.new(:atoll, coordinate_3)
+    {:ok, l_shape_island} = Island.new(:l_shape, coordinate_4)
+
+    board =
+      Board.new()
+      |> Board.position_island(:dot, dot_island)
+      |> Board.position_island(:square, square_island)
+      |> Board.position_island(:atoll, atoll_island)
+      |> Board.position_island(:l_shape, l_shape_island)
+
+    assert false == Board.all_islands_positioned?(board)
+  end
+
+  test "five islands are enough to start the game" do
+    {:ok, coordinate_1} = Coordinate.new(1, 1)
+    {:ok, coordinate_2} = Coordinate.new(1, 2)
+    {:ok, coordinate_3} = Coordinate.new(1, 4)
+    {:ok, coordinate_4} = Coordinate.new(1, 6)
+    {:ok, coordinate_5} = Coordinate.new(3, 1)
+    {:ok, dot_island} = Island.new(:dot, coordinate_1)
+    {:ok, square_island} = Island.new(:square, coordinate_2)
+    {:ok, atoll_island} = Island.new(:atoll, coordinate_3)
+    {:ok, l_shape_island} = Island.new(:l_shape, coordinate_4)
+    {:ok, s_shape_island} = Island.new(:s_shape, coordinate_5)
+
+    board =
+      Board.new()
+      |> Board.position_island(:dot, dot_island)
+      |> Board.position_island(:square, square_island)
+      |> Board.position_island(:atoll, atoll_island)
+      |> Board.position_island(:l_shape, l_shape_island)
+      |> Board.position_island(:s_shape, s_shape_island)
+
+    assert true == Board.all_islands_positioned?(board)
+  end
+
+  test "missed hit can't win nor forest the island" do
+    {:ok, coordinate_1} = Coordinate.new(1, 1)
+    {:ok, coordinate_2} = Coordinate.new(1, 3)
+
+    {:ok, square_island} = Island.new(:square, coordinate_1)
+
+    board =
+      Board.new()
+      |> Board.position_island(:square, square_island)
+
+    assert {:miss, :none, :no_win, ^board} = Board.guess(board, coordinate_2)
   end
 end
